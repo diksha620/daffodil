5a853a6d19ac277d04bf8f9a10f1d92ab191c4e0
diksha khandelwal <dikshakhandelwal0612@gmail.com>
2022-02-04 13:25:50 +0530

    index file added

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('hi');
+const message = {
+    glossary: {
+      title: 'example glossary',
+      GlossDiv: {
+        title: 'S',
+        GlossList: {
+          GlossEntry: {
+            ID: 'SGML',
+            SortAs: 'SGML',
+            GlossTerm: 'Standard Generalized Markup Language',
+            Acronym: 'SGML',
+            Abbrev: 'ISO 8879:1986',
+            GlossDef: {
+              para: 'A meta-markup language, used to create markup languages such as DocBook.',
+              GlossSeeAlso: ['GML', 'XML'],
+            },
+            GlossSee: 'markup',
+          },
+        },
+      },
+    },
+  };
+
+  var a  = JSON.parse(JSON.stringify(message));
+  a.glossary.GlossDiv.GlossList.GlossEntry.GlossSee = 'hi';
+  
+console.log(message);
